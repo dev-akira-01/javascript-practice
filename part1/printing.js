a220f019ac19eeda48cd96a2617bbbc8240e628f
Akira <akira.dev.tech@gmail.com>
2026-04-17 10:41:27 +0530

    update printing.js / different ways of printing

---
+++ part1/printing.js
@@ -0,0 +1,13 @@
+// has a line break
+
+// console.log("Hello!");
+// console.log("Hello! 2");
+
+// Prints output on the same line when given to print multiple outputs
+
+// process.stdout.write("Hello A!");
+// process.stdout.write("Hello A!");
+
+// print table
+
+// console.table({ city: "Guwahati", state: "Assam" });
