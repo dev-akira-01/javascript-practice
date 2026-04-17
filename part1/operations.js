1b2e1c7b8665e8e95789ab8cb208ffae72d50754
Akira <akira.dev.tech@gmail.com>
2026-04-17 15:55:37 +0530

    add operations.js , different sorts of operations using operators

---
+++ part1/operations.js
@@ -0,0 +1,13 @@
+// let score = 175;
+// let bonus = 25;
+
+// let totalScore = score + bonus;
+
+// console.log("The total score is ", totalScore);
+
+// let addition = 175 + 25; // add
+// let substract = 175 - 25; // substract
+// let mult = 25 * 4; // multiply
+// let div = 25 / 5; // divide
+// let reminder = 26 % 5; // reminder
+// let expo = 2 ** 4; // exponent
