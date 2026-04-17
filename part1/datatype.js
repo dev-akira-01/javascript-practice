f5046b1f9bf10db1e2721ee472d322870a70e72a
Akira <akira.dev.tech@gmail.com>
2026-04-17 11:07:24 +0530

    update datatype.js, printing variables, storing values of variables in other variables

--- part1/datatype.js
+++ part1/datatype.js
@@ -17,12 +17,18 @@ Symbol
 
 // var score = 102 old way
 
-let score = 102;
-let name = "Akira";
-let isLoggedin = false;
+let score = 102; // number
+
+let name = "Akira"; // string
+
+let isLoggedin = false; //boolean
 
 // Object
 
-let teaTypes = ["Lemon Tea", "Orange Tea", "Black Tea"];
+let teaTypes = ["Lemon Tea", "Orange Tea", "Black Tea"]; // array
 
-let user = { firstname: "Akira", lastname: "Dev" };
+let user = { firstname: "Akira", lastname: "Dev" }; // dictionary
+
+let getScore = score; // borrow values from other variables
+
+console.log(getScore);
