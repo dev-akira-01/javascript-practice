b839e3430ca966b796fc9cf1a481bbc71134e49a
Akira <akira.dev.tech@gmail.com>
2026-04-17 11:31:21 +0530

    add changes.js, how to overwrite a value in variable and const variable which cannot be overwritten

---
+++ part1/changes.js
@@ -0,0 +1,11 @@
+let gameName = "arknights";
+
+gameName = "arknights endfield"; // overwriting the value of var gameName
+
+console.log(gameName);
+
+const username = "akira-dev-01";
+
+username = "akira-dev";
+
+console.log(username); // type error, const variable cannot be changed
