a3661fd74bac15b9fea517cfe08fee7aa5fce2ab
Akira <akira.dev.tech@gmail.com>
2026-04-17 12:08:23 +0530

    update practice1.js , number dtype

--- part1/practice1.js
+++ part1/practice1.js
@@ -1,4 +1,18 @@
+/*
+
+// 1. String
+
 let name = "Akira";
 // let greetings = "Hello, " + name + "!";
 
 console.log("Hello, ", name + "!"); // works the same
+
+*/
+
+// 2. Number
+
+let myAge = 18;
+
+let hisAge = 24;
+
+console.log("Sum of our age is", myAge + hisAge);
