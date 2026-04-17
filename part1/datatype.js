caeb3f3a0ab3ec3b06f1946aea6b88c51955ff15
Akira <akira.dev.tech@gmail.com>
2026-04-17 10:59:08 +0530

    datatyes objects and variables

---
+++ part1/datatype.js
@@ -0,0 +1,28 @@
+// Datatypes in JS
+
+/*
+
+String
+Number
+Boolean
+Bigint
+
+Undefined
+Null
+
+Object
+Symbol
+
+*/
+
+// var score = 102 old way
+
+let score = 102;
+let name = "Akira";
+let isLoggedin = false;
+
+// Object
+
+let teaTypes = ["Lemon Tea", "Orange Tea", "Black Tea"];
+
+let user = { firstname: "Akira", lastname: "Dev" };
