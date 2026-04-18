cd50fec827046ebdf9373517b5acfc92a5643bba
Akira <akira.dev.tech@gmail.com>
2026-04-18 15:09:34 +0530

    add non-primitve.js, objects and object manipulation

---
+++ part2/non-primitives.js
@@ -0,0 +1,17 @@
+//objects
+
+// let userName = {
+//   firstName: "Akira",
+//   isLoggedIn: true,
+// };
+
+// userName.lastName = "Dev";
+
+// console.log(userName);
+// console.log(userName.lastName);
+// console.log(typeof userName);
+
+// let today = new Date();
+// console.log(today.getUTCDate());
+
+// array
