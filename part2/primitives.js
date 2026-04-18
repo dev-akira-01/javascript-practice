2a53f1bad84048dfee250a4bc01477b946efe93a
Akira <akira.dev.tech@gmail.com>
2026-04-18 14:40:44 +0530

    add primitives.js, string interpolation

---
+++ part2/primitives.js
@@ -0,0 +1,28 @@
+// number
+
+// let num1 = 120;
+// let num2 = new Number(120);
+
+// console.log(num1);
+// console.log(typeof num2);
+
+// console.log(typeof num1);
+
+// let isUser = true; //boolean
+
+// null and undefined
+
+// let firstName; //undefined
+// let firstName = null; //null
+// let firstName = undefined; //undefined
+
+// console.log(firstName);
+
+// string
+
+let greeting = "Hello";
+let name = "Akira";
+let greetMessage = "How is your day going";
+
+let message = `${greeting}! ${name}, ${greetMessage}?`;
+console.log(message);
