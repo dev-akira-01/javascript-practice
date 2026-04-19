6778e9593b7f7ed080a2372fc8075af30dd05714
Akira <akira.dev.tech@gmail.com>
2026-04-19 16:19:32 +0530

    add conditions.js , conditional statements if and else

---
+++ part3/conditions.js
@@ -0,0 +1,53 @@
+// check if a number is greater than another number
+
+// let num1 = 5;
+// let num2 = 10;
+
+// if (num1 > num2) {
+//   console.log("num1 is greater than num2");
+// } else {
+//   console.log("num2 is greater than num1");
+// }
+
+// check if a string is equal to another string
+
+// let user1 = "akira";
+
+// let user2 = "aki";
+
+// if (user1 == user2) {
+//   console.log("username already exists");
+// } else {
+//   console.log("username changed!");
+// }
+
+// checking if a variable is a number or not
+
+// let var1 = 20;
+// let var2 = "20";
+
+// if (typeof var2 === "number") {
+//   console.log("variable is a number");
+// } else {
+//   console.log("variable is a string");
+// }
+
+// checking if a boolean value is true or false
+
+// let boo1 = false;
+
+// if (boo1) {
+//   console.log("boo1 is true");
+// } else {
+//   console.log("boo1 is false");
+// }
+
+// check if array is empty or not
+
+let items = [];
+
+if (items.lenth === 0) {
+  console.log("Array is empty");
+} else {
+  console.log("Array is not empty");
+}
