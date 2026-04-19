1ff0458e69c8bf51703a378b6ec393e8c60844e9
Akira <akira.dev.tech@gmail.com>
2026-04-19 17:12:25 +0530

    add array.js, array methods

---
+++ part3/array.js
@@ -0,0 +1,15 @@
+/* access the first element in the array and store in another variable*/
+
+/* let teaFlavors = ["green tea", "black tea", "oolong tea"];
+
+const firstTea = teaFlavors[0];
+ 
+console.log(firstTea); */
+
+// arr cities, access the third element
+
+/* let cities = ["London", "Paris", "Tokyo", "New York"];
+
+const favouriteCity = cities[2];
+
+console.log(favouriteCity); */
