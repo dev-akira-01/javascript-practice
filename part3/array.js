097343eb18d514440d9147253fe34214867bf60a
Akira <akira.dev.tech@gmail.com>
2026-04-19 18:11:25 +0530

    update array.js

--- part3/array.js
+++ part3/array.js
@@ -33,6 +33,48 @@ console.log(citiesVisited); */
 
 // pop the last array item
 
-let teaOrders = ["Chai", "Iced Tea", "Matcha", "Earl Grey", "Milk Tea"];
+/* let teaOrders = ["Chai", "Iced Tea", "Matcha", "Earl Grey", "Milk Tea"];
 let lastOrder = teaOrders.pop();
-console.log(lastOrder);
+console.log(lastOrder); */
+
+//create a soft copy of an array
+
+/* let popularTeas = ["Green Tea", "Oolong Tea", "Chai"];
+let softCopy = popularTeas;
+popularTeas.push("berlin");
+popularTeas = ["Green Tea", "Oolong Tea"];
+console.log(softCopy);
+console.log(popularTeas); */
+
+// make a hard copy of array
+
+/* let topCities = ["Tokyo", "Berlin", "Singapore", "New York"];
+let hardCopyCities = [...topCities];
+// let hardCopyCities = topCities.slice();
+topCities.pop();
+console.log(topCities);
+console.log(hardCopyCities);
+ */
+
+// merging two arrays
+
+/* let europeanCities = ["Paris", "Rome"];
+let asianCities = ["Tokyo", "Bangkok"];
+
+// const worldCities = europeanCities + "," + asianCities;
+let worldCities = europeanCities.concat(asianCities);
+
+console.log(worldCities); */
+
+// find the array length and store in another  variable
+
+/* let teaMenu = ["Masala Tea", "Oolang Tea", "Green Tea", "Milk Tea"];
+menuLength = teaMenu.length;
+
+console.log(menuLength); */
+
+/* let cityBucketList = ["Kyoto", "Akihabara", "Shibuya", "Tokyo", "Harajuku"];
+
+const isTokyoInArr = cityBucketList.includes("Tokyo");
+console.log(isTokyoInArr);
+ */
