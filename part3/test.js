338ed7fb4232114589eec9567a2470a1219cf6dd
Akira <akira.dev.tech@gmail.com>
2026-04-20 13:45:04 +0530

    add test.js, arr methods

---
+++ part3/test.js
@@ -0,0 +1,10 @@
+/* let arr = ["a", "b", "c"];
+console.log(arr.length - 1); // last element's index
+ */
+
+/* let arr = ["Tokyo", "Akihabara", "Shibuya"];
+
+console.log(arr["2"]); // with quotes
+console.log(arr[2]); //without quotes
+
+//same same */
