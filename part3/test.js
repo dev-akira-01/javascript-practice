061cbccf3b9ba840b1e06668a5848bc46205a6f8
Akira <akira.dev.tech@gmail.com>
2026-04-20 14:17:01 +0530

    update test,.js, more arr methods

--- part3/test.js
+++ part3/test.js
@@ -8,3 +8,39 @@ console.log(arr["2"]); // with quotes
 console.log(arr[2]); //without quotes
 
 //same same */
+
+/* const fruits = ["apple", "banana", "mango"];
+fruits[7] = "orange";
+fruits.length = 10;
+//console.log(fruits.length);
+//console.log(fruits);
+//console.log(Object.keys(fruits)); // non-empty indexes
+
+console.log(fruits[5]); //undefined */
+
+/* const cities = ["tokyo", "fukuoka", "harajuku"];
+
+let strings = cities.join(); // creates string from an array
+console.log(typeof strings); */
+
+/* const fruits = ["Apple", "Banana"];
+const newLength = fruits.push("Orange", "Guava"); //adds new items to the arr
+
+console.log(fruits);
+console.log(newLength); */
+
+/* const fruits = ["Apple", "Banana", "Strawberry", "Mango", "Cherry"];
+const start = -3;
+const removeItems = fruits.splice(start);
+
+console.log(fruits);
+console.log(removeItems); */
+
+/* const fruits = ["Apple", "Banana", "Strawberry", "Mango", "Cherry"];
+const start = -3;
+const removedItems = fruits.splice(start);
+console.log(fruits);
+// ["Apple", "Banana"]
+console.log(removedItems);
+// ["Strawberry", "Mango", "Cherry"]
+ */
