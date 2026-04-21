0d03ce5f2d5e9733f24c81206d858b2d5575418f
Akira <akira.dev.tech@gmail.com>
2026-04-21 12:53:07 +0530

    update loops.js

--- part4/loops.js
+++ part4/loops.js
@@ -20,7 +20,7 @@ console.log(sum); */
 
 // i want 1+2+3+4+5
 
-let sum = 0;
+/* let sum = 0;
 let i = 1;
 
 while (i <= 5) {
@@ -28,4 +28,50 @@ while (i <= 5) {
   i++;
 }
 
-console.log(sum);
+console.log(sum); */
+
+// while loop for storing 5,4,3,2,1 in an array
+
+let count = [];
+let i = 5;
+
+while (i >= 1) {
+  count.push(i);
+  i--;
+}
+
+// console.log(count);
+
+/* let teaCollection = [];
+let tea;
+
+do {
+  tea = prompt(`Enter your favourite tea(Type type to finish)`);
+
+  if (tea !== "stop") {
+    teaCollection.push(tea);
+  }
+} while (tea !== "stop");
+
+console.log(teaCollection); */
+
+/* total = 0;
+i = 1;
+
+do {
+  total += i;
+  i++;
+} while (i <= 3);
+
+console.log(total);
+ */
+
+/* let multipliedNumbers = [];
+let numbers = [2, 4, 6];
+
+for (let i = 0; i < numbers.length; i++) {
+  takeNumber = numbers[i] * 2;
+  multipliedNumbers.push(takeNumber);
+}
+
+console.log(multipliedNumbers); */
