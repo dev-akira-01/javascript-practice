28ab53fd4e1d66f1816070c8f9997188a151c78b
Akira <akira.dev.tech@gmail.com>
2026-04-21 18:37:53 +0530

    add test1.js

---
+++ part4/test1.js
@@ -0,0 +1,32 @@
+// given an array of numbers. Create a new array where each number is increased by 5.
+
+/* let numbers = [1, 3, 5, 7];
+
+// expected [6,8,10,12]
+
+for (let index = 0; index < numbers.length; index++) {
+  numbers[index] += 5;
+} */
+
+// console.log(numbers);
+
+// array that only contains even numbers multiplied by 3.
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let evenNumbers = [];
+let multipliedNumbers = [];
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 === 0) {
+    evenNumbers.push(numbers[i]);
+  }
+}
+console.log(evenNumbers);
+
+for (let index = 0; index < evenNumbers.length; index++) {
+  const multiply = evenNumbers[index] * 3;
+  multipliedNumbers.push(multiply);
+}
+
+console.log(multipliedNumbers);
+// expected [6,12,18,24]
