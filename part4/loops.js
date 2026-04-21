19c04e33fe2aac2965792608529319ba21dfb31a
Akira <akira.dev.tech@gmail.com>
2026-04-21 11:16:39 +0530

    loops.js

---
+++ part4/loops.js
@@ -0,0 +1,31 @@
+// while loop
+// while/do loop
+//for loop
+
+// 1
+// while loop - calculates the sum of all numbers from 1 to 5 and stores the result in a variable named 'sum'
+
+/* let sum = 0;
+let count = 0; // iteration
+
+while (count <= 5) {
+  sum = sum + count;
+  //   sum += 1;
+  count++;
+}
+
+console.log(sum); */
+
+//retry
+
+// i want 1+2+3+4+5
+
+let sum = 0;
+let i = 1;
+
+while (i <= 5) {
+  sum += i;
+  i++;
+}
+
+console.log(sum);
